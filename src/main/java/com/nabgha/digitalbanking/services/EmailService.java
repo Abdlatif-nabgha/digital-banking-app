576250eb4e2cae24f9f985784fc1e1aecac0a6c4
abdlatif-nabgha <abdelatif.nabgha06@gmail.com>
2026-05-04 22:39:15 +0100

    add asyn to email

--- src/main/java/com/nabgha/digitalbanking/services/EmailService.java
+++ src/main/java/com/nabgha/digitalbanking/services/EmailService.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+@Async
 @Service
 @RequiredArgsConstructor
 public class EmailService {
